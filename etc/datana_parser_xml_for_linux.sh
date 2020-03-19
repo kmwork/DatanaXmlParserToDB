db3e2685ef91530f77047f8673a769d3323a7d2d
Konstantin Kakunin <kmwork@yandex.ru>
2020-03-19 19:04:30 +0300

    получилась сборка jar (springboot)

--- etc/datana_parser_xml_for_linux.sh
+++ etc/datana_parser_xml_for_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin;$PATH
-java -Dapp.dir="/home/lin/apps/datana_siemens_v1" -Dfile.encoding="UTF8" -jar Siemens-K4-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -Dapp.dir="/home/lin/apps/datana_xml_parser" -Dfile.encoding="UTF8" -jar S7ParserXML-K6-1-alfa-spring-boot.jar
