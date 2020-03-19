46e5051d3cf19f16b4412c8c3923648b0e220ac5
Konstantin Kakunin <kmwork@yandex.ru>
2020-03-19 13:51:16 +0300

    перевод конфига на yaml

--- src/main/java/ru/datana/steel/parser/config/AppConst.java
+++ src/main/java/ru/datana/steel/parser/config/AppConst.java
@@ -8,7 +8,7 @@ public class AppConst {
     public static final String CONF_FILE_NAME = "datana_xml_parser.properties";
     public static final String SYS_DIR_PROP = "app.dir";
     public static final String ENCODING = "UTF8";
-    public final static String EXT_CONFIG_NAME = "datana-application.yaml";
+    public final static String EXT_CONFIG_NAME = "datana-xml-parser-config.yaml";
 
     public static final String SUCCESS_LOG_PREFIX = "[App-Успешно] ";
     public static final String ERROR_LOG_PREFIX = "[App-Ошибка] ";
