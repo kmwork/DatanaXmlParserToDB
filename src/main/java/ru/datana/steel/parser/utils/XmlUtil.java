a31b439b5b3684eba6268aad50f2f93b5ecbeaa7
Konstantin Kakunin <kmwork@yandex.ru>
2020-03-19 20:41:47 +0300

    fix with xml

--- src/main/java/ru/datana/steel/parser/utils/XmlUtil.java
+++ src/main/java/ru/datana/steel/parser/utils/XmlUtil.java
@@ -2,15 +2,10 @@ package ru.datana.steel.parser.utils;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import ru.datana.steel.parser.config.AppConst;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamReader;
 import java.io.File;
-import java.io.FileReader;
 
 @Slf4j
 public class XmlUtil {
@@ -24,14 +19,11 @@ public class XmlUtil {
     private int failCount = 0;
 
     public <XML_CLASS> XML_CLASS xmlFileToObject(File xmlFile, Class<XML_CLASS> clazz) throws AppException {
-        try (FileReader fileReader = new FileReader(xmlFile, AppConst.ENCODING)) {
+        try {
             log.info(PREFIX_LOG + " Начало разбора файла " + xmlFile.getAbsolutePath() + " для класса " + clazz.getSimpleName());
-            XMLInputFactory factory = XMLInputFactory.newInstance();
-            XMLStreamReader xmlStream = factory.createXMLStreamReader(fileReader);
             JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            JAXBElement<XML_CLASS> jaxbElement = unmarshaller.unmarshal(xmlStream, clazz);
-            XML_CLASS result = jaxbElement.getValue();
+            XML_CLASS result = (XML_CLASS) unmarshaller.unmarshal(xmlFile);
             log.info(PREFIX_LOG + "Файл разобран как объект класса " + result.getClass().getSimpleName());
             log.trace(PREFIX_LOG + "result = " + result.toString());
 
