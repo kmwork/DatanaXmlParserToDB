b96a6c293521911326b641c996993bb97a1f237b
Konstantin Kakunin <kmwork@yandex.ru>
2020-03-19 15:02:14 +0300

    перевод конфига на yaml

--- src/main/java/ru/datana/steel/parser/config/LanitSpringConfig.java
+++ src/main/java/ru/datana/steel/parser/config/LanitSpringConfig.java
@@ -3,6 +3,7 @@ package ru.datana.steel.parser.config;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,6 +12,7 @@ import javax.annotation.PostConstruct;
 
 
 @Configuration
+@EnableBatchProcessing
 @ConfigurationProperties(prefix = "datana.global")
 @Slf4j
 @EnableTransactionManagement
