9365e4475421b071262cfe7296bda7d7b82d112b
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-03-23 17:20:14 +0300

    fix shell

--- etc/datana_parser_xml_for_linux.sh
+++ etc/datana_parser_xml_for_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=/home/lin/apps/jdk13
-export PATH=$JAVA_HOME/bin;$PATH
+export PATH=$JAVA_HOME/bin:$PATH
 java java -Dapp.profile="remote_postgres" -Dapp.dir="/home/lin/apps/datana_xml_parser" -Dfile.encoding="UTF8" -jar S7ParserXML-K6-1-alfa-spring-boot.jar
